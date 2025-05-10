84bab8697ec0e593b4b1206df237655446ff5ad4
Anakha-18 <167894220+Anakha-18@users.noreply.github.com>
2025-05-10 17:35:47 +0530

    Update HollowRectanglePattern.java

--- HollowRectanglePattern.java
+++ HollowRectanglePattern.java
@@ -5,6 +5,7 @@ public class HollowRectanglePattern {
 
         //outer loop
         for(int i=1; i<=n; i++){
+            //inner loop
             for(int j=1; j<=5; j++){
                 if(i==1 || i==n || j==1 || j==m){
                     System.out.print("*");
@@ -12,7 +13,7 @@ public class HollowRectanglePattern {
                     System.out.print(" ");
                 }
             }
-            System.out.println();
+            System.out.println(); 
         }
     }
 }
